71d9f8a435fa608d6e88c2b25441d03f4c3e42fb
poq79 <273589250@qq.com>
2024-03-25 14:01:11 +0800

    perf: screenshot模块多处优化改进

--- web/screenshot.go
+++ web/screenshot.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/chromedp/cdproto/page"
 	"os"
 	"strings"
 	"sync"
@@ -58,13 +59,14 @@ type ScreenshotTask struct {
 }
 
 type ScreenshotServer struct {
-	Cfg       *ScreenshotCfg
-	ctx       context.Context
-	state     uint32
-	Avaliable bool
-	chrome    []chromedp.ExecAllocatorOption
-	queue     chan *ScreenshotTask
-	Logger    vela.Log
+	Cfg               *ScreenshotCfg
+	ctx               context.Context
+	navigateWaitgroup sync.WaitGroup
+	state             uint32
+	Avaliable         bool
+	chrome            []chromedp.ExecAllocatorOption
+	queue             chan *ScreenshotTask
+	Logger            vela.Log
 }
 
 var (
@@ -107,6 +109,13 @@ func NewScreenServer(ctx context.Context, cfg *ScreenshotCfg, log vela.Log) (*Sc
 }
 
 func (st *ScreenshotServer) Start() error {
+	level := st.Logger.LoggerLevel()
+	if st.Cfg.Debug {
+		_ = level.Set("debug")
+	} else {
+		_ = level.Set("error")
+	}
+	st.Logger.Infof("[+]ScreenshotServer starting...")
 	st.queue = make(chan *ScreenshotTask)
 	for i := 0; i < st.Cfg.Thread; i++ {
 		go st.navigate(i, st.chrome)
@@ -115,12 +124,14 @@ func (st *ScreenshotServer) Start() error {
 }
 
 func (st *ScreenshotServer) navigate(workerNum int, option []chromedp.ExecAllocatorOption) {
-
-	pCtx, pCancel := chromedp.NewExecAllocator(st.ctx, option...)
-	defer pCancel()
-
-	ctx, cancel := chromedp.NewContext(pCtx) // chrome tab ctx
+	st.navigateWaitgroup.Add(1)
+	defer st.navigateWaitgroup.Done()
+	sctx, sCancel := context.WithCancel(context.Background())
+	pCtx, pCancel := chromedp.NewExecAllocator(sctx, option...)
+	ctx, cancel := chromedp.NewContext(pCtx) // Chrome tab ctx
 	defer cancel()
+	defer pCancel()
+	defer sCancel()
 
 	// Check & Make Browser not close
 	if e := chromedp.Run(ctx, chromedp.Navigate("about:blank")); e != nil {
@@ -133,19 +144,60 @@ func (st *ScreenshotServer) navigate(workerNum int, option []chromedp.ExecAlloca
 		return
 	}
 
+	switchToBlank := func() error {
+		nCtx, nCancel := context.WithTimeout(ctx, time.Duration(3)*time.Second)
+		defer nCancel()
+		err := chromedp.Run(nCtx, chromedp.Navigate("about:blank"))
+		if err != nil {
+			nCancel()
+			// 如果超时则重新打开一个新的
+			cancel()
+			ctx, cancel = chromedp.NewContext(pCtx) // Chrome tab ctx
+			n2Ctx, n2Cancel := context.WithTimeout(ctx, time.Duration(3)*time.Second)
+			defer n2Cancel()
+			err = chromedp.Run(n2Ctx, chromedp.Navigate("about:blank"))
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return nil
+	}
+
 	screen := func(target *ScreenshotTask) error {
 		var buf []byte
+		st.Logger.Debugf("[+]ScreenshotServer [debug 169] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 		target.TargetCtx, target.TimeoutCancel = context.WithTimeout(ctx, time.Duration(st.Cfg.Timeout)*time.Second)
+		defer target.TimeoutCancel()
+		st.Logger.Debugf("[+]ScreenshotServer [debug 172] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
+		chromedp.ListenTarget(target.TargetCtx, func(ev interface{}) {
+			if ev, ok := ev.(*page.EventJavascriptDialogOpening); ok {
+				st.Logger.Infof("[+]ScreenshotServer navigate [%d] closing alert [%s] (URL:%s)", workerNum, ev.Message, target.Url)
+				go func() {
+					if err := chromedp.Run(ctx,
+						page.HandleJavaScriptDialog(true),
+					); err != nil {
+						st.Logger.Errorf("[-] Failed to take (navigate: %d URL:%s) screenshot(HandleJavaScriptDialog): %v", workerNum, target.Url, err)
+					}
+				}()
+			}
+		})
 		if err := chromedp.Run(target.TargetCtx, fullScreenshot(target.Url, 100, &buf)); err != nil {
-			st.Logger.Errorf("[-] Failed to take (URL:%s) screenshot: %v", target.Url, err)
+			st.Logger.Errorf("[-] Failed to take (navigate: %d URL:%s) screenshot: %v", workerNum, target.Url, err)
 			target.Done <- -1
-			chromedp.Run(ctx, chromedp.Navigate("about:blank"))
+			st.Logger.Debugf("[+]ScreenshotServer [debug 188] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
+			swerr := switchToBlank()
+			if swerr != nil {
+				st.Logger.Errorf("[-] Failed to take (navigate: %d URL:%s) screenshot: (switchToBlank EEROR)%v", swerr)
+				return err
+			}
+			st.Logger.Debugf("[+]ScreenshotServer [debug 194] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 			return err
 		}
-
+		st.Logger.Debugf("[+]ScreenshotServer [debug 197] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 		ScreenshotURL, err := util.UploadToMinio(st.Cfg.Miniocfg, target.Radartaskid+".png", bytes.NewReader(buf), int64(len(buf)))
 		if err != nil {
-			st.Logger.Errorf("[-] Failed to write file %v", err)
+			st.Logger.Errorf("[-] Failed to upload to minio %v", err)
 			target.Done <- -1
 			return err
 		}
@@ -153,32 +205,36 @@ func (st *ScreenshotServer) navigate(workerNum int, option []chromedp.ExecAlloca
 
 		if st.Cfg.Save {
 			pngFilePath := fmt.Sprintf("./%s/%s.png", st.Cfg.ResultDir, target.Radartaskid)
-			if e := os.WriteFile(pngFilePath, buf, 0644); e != nil {
+			if err = os.WriteFile(pngFilePath, buf, 0644); err != nil {
 				st.Logger.Errorf("[-] Failed to write file %v", err)
-				return e
+				target.Done <- -1
+				return err
 			}
 		}
-
+		st.Logger.Debugf("[+]ScreenshotServer [debug 214] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 		if ctx.Err() != nil {
-			st.Logger.Infof("[+] screenshot failed, timeout! %s", target.Url)
+			st.Logger.Errorf("[+] screenshot (URL:%s) failed, ctx error: %s", target.Url, ctx.Err())
 			// target.Wg.Done()
+			ctx, cancel = chromedp.NewContext(pCtx)
 			target.Done <- -1
-			chromedp.Run(ctx, chromedp.Tasks{chromedp.Navigate("about:blank")})
+			//_ = chromedp.Run(ctx, chromedp.Tasks{chromedp.Navigate("about:blank")})
 			return err
 		}
+		st.Logger.Debugf("[+]ScreenshotServer [debug 223] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
+		_ = chromedp.Run(ctx, chromedp.Tasks{chromedp.Navigate("about:blank")})
+		st.Logger.Debugf("[+]ScreenshotServer [debug 225] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 
-		chromedp.Run(ctx, chromedp.Tasks{chromedp.Navigate("about:blank")})
 		target.Done <- 1
+		st.Logger.Debugf("[+]ScreenshotServer [debug 228] navigate [%d] start screen (URL:%s)", workerNum, target.Url)
 		return nil
 	}
 
 	for target := range st.queue {
-		screen(target)
+		st.Logger.Debugf("[+]ScreenshotServer navigate [%d] start screen (URL:%s)", workerNum, target.Url)
+		_ = screen(target)
+		st.Logger.Debugf("[+]ScreenshotServer navigate [%d] end screen  (URL:%s)", workerNum, target.Url)
 	}
-	if st.Cfg.Debug {
-		st.Logger.Infof("[+]ScreenshotServer navigate [%d] closed", workerNum)
-	}
-	pCtx.Done()
+	st.Logger.Infof("[+]ScreenshotServer navigate [%d] closed", workerNum)
 }
 
 func (st *ScreenshotServer) Push(target *ScreenshotTask) {
@@ -193,24 +249,17 @@ func (st *ScreenshotServer) Close() {
 			st.Logger.Errorf("ScreenshotServer Closer ERR:%v", e)
 		}
 	}()
-	if st.queue == nil || len(st.queue) == 0 {
+	if st.queue == nil {
+		st.Logger.Infof("ScreenshotServer queue为nil")
 		return
 	}
-	close(st.queue)
-	for {
-		if len(st.queue) == 0 {
-			targets, err := chromedp.Targets(st.ctx)
-			if err != nil {
-				st.Logger.Infof("[+]ScreenshotServer closed (chromedp targets get err)")
-				break
-			}
-			if len(targets) == 0 {
-				st.Logger.Infof("[+]ScreenshotServer closed")
-				break
-			}
-		}
-		time.Sleep(time.Second * 1)
+	if len(st.queue) == 0 {
+		st.Logger.Infof("ScreenshotServer queue队列为空")
 	}
+	close(st.queue)
+	st.Logger.Infof("ScreenshotServer queue closed")
+	st.navigateWaitgroup.Wait()
+	st.Logger.Infof("ScreenshotServer ended")
 }
 
 func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
@@ -218,7 +267,7 @@ func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
 		chromedp.Navigate(urlstr),
 		//chromedp.WaitVisible("style"),
 		chromedp.WaitReady("body"),
-		chromedp.Sleep(3 * time.Second),
+		chromedp.Sleep(1 * time.Second),
 		//chromedp.OuterHTML(`document.querySelector("body")`, &htmlContent, chromedp.ByJSPath),
 		chromedp.FullScreenshot(res, quality),
 	}
